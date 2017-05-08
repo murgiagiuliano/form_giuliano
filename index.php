ea87ed2ff53e8a0e2bd4e2b0a435e42f2c6c0836
GiulianoMurgia <andrea.concas.sc@gmail.com>
2017-05-08 11:17:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+    <div class="jumbotron">
+      <div class="container">
+      <?php include('fragments/navbar.html'); ?>
+      <?php include('fragments/welcome.html'); ?>
+      </div>
+    </div>
+    <?php include('fragments/form.html'); ?>
+</div>
+</div>
+<script src="vendors/js/jquery.min.js"></script>
+<script src="vendors/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/js/bootstrapValidator.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
